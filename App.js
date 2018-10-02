4c9dea0eb8e90f57806f204e90325774ec54c238
Renan Garcia <email@renangarcia.me>
2018-10-02 16:56:31 -0300

    Corrige a cor de fundo da tela inicia

--- App.js
+++ App.js
@@ -34,7 +34,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: '#cecece',
   },
   welcome: {
     fontSize: 20,
